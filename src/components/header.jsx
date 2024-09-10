8e10cdd798990f9d76ed57341d8cfc7195df1f19
LuizFelipe <luiz.felipesouza0306@gmail.com>
2024-09-10 14:09:30 -0300

    initial commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import styles from './header.module.css'
 
 export function Header() {
     return (
-        <div className={styles.fundo}>
+        <div className={styles.header}>
         <img src={buscAnime}/>
         </div>
     )
