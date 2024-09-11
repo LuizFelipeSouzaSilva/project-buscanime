27720eabb4b4f26dca2d9b7973de300a5a8ff793
LuizFelipe <luiz.felipesouza0306@gmail.com>
2024-09-11 13:44:02 -0300

    Fazendo o serach fazer busca por texto

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,13 +1,24 @@
-import styles from './search.module.css'
+import styles from './search.module.css';
 
-export function Search() {
-    return ( 
+export function Search({ onSearchChange, onSearchClick }) {
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      onSearchClick(); 
+    }
+  };
+
+  return (
+
     <div className={styles.Search}>
-        <input type="text" 
-        placeholder="Digite algo aqui..." 
-        />
-        <button>Buscar</button>
+      <input
+        type="text"
+        placeholder="Digite algo aqui..."
+        onChange={onSearchChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={onSearchClick}>Buscar</button>
     </div>
-
-    )
+    
+  );
 }
